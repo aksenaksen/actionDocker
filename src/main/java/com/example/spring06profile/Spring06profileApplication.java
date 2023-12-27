26fe18cd70b21d4ddce0263c462a151fca62c6d0
aksenaksen <ans109905@naver.com>
2023-12-27 11:00:26 +0900

    first

---
+++ src/main/java/com/example/spring06profile/Spring06profileApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring06profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06profileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring06profileApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/spring06profile/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.spring06profile.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @Value("${server.servlet.context-path}") //application properties or yml 파일에 있는 값을 얻어올수있다.
+
+    String path;
+
+    @GetMapping("/main")
+    public String main(){
+        System.out.println("path: "+path);
+        return "hello";
+    }
+
+
+
+}
